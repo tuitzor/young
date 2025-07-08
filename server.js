5808990a93096c5d6c931f3552dfea6e2ba26c4f
Эдуард <zhuravleveduard16@gmail.com>
2025-07-09 01:10:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -10,22 +10,25 @@ const wss = new WebSocket.Server({ server });
 // Статическая раздача фронтенда
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Хранилище ответов
-const responses = new Map();
+// Хранилище скриншотов и ответов
+const screenshots = new Map();
 
 wss.on('connection', (ws) => {
   console.log('Client connected');
   ws.on('message', (message) => {
-    console.log('Received message:', message);
+    console.log('Received:', message);
     const data = JSON.parse(message);
     if (data.type === 'screenshot') {
-      const questionId = data.questionId;
-      console.log(`Received screenshot with questionId: ${questionId}`);
-      // Симуляция обработки и генерации ответа
-      const answer = `Response for screenshot ${questionId.split('-')[0]}`;
-      responses.set(questionId, answer);
+      const { questionId, screenshot } = data;
+      screenshots.set(questionId, screenshot);
+      // Симуляция ответа
+      const answer = `Processed screenshot ${questionId.split('-')[0]}`;
       ws.send(JSON.stringify({ type: 'answer', questionId, answer }));
       console.log(`Sent answer: ${answer} for questionId: ${questionId}`);
+    } else if (data.type === 'userAnswer') {
+      const { questionId, answer } = data;
+      screenshots.set(questionId, { ...screenshots.get(questionId), userAnswer: answer });
+      ws.send(JSON.stringify({ type: 'answer', questionId, answer: `User answer received: ${answer}` }));
     } else if (data.type === 'pageHTML') {
       console.log('Received page HTML');
     }
