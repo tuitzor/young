fc275057b22ab8806b34c09ac7c53b4cc4ea5127
Эдуард <zhuravleveduard16@gmail.com>
2025-07-16 16:40:49 +0500

    Update helper.js

--- public/helper.js
+++ public/helper.js
@@ -308,7 +308,7 @@
             const parts = filename.split("-");
             const index = parts[parts.length - 1].replace(".png", "");
             answerElement.innerHTML = `
-                <h3 style="font-size: 16px; margin-bottom: 4px;">ответ ${index}:</h3>
+                <h3 style="font-size: 16px; margin-bottom: 4px;">k :</h3>
                 <p style="font-size: 12px;">${data.answer || "Нет ответа"}</p>
             `;
             answerWindow.appendChild(answerElement);
